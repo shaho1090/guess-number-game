1c71d352702aa56353cfce6aa06ca9d2e793cbec
shaho1090 <shaho.parvini@gmail.com>
2024-09-14 17:20:35 +0330

    lesson: 55, add linear gradient

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     borderRadius: 8,
     elevation: 10,
     shadowColor: "black",
