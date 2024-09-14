ce32137762d2359600dcc9aa9274de0d79ec6d71
shaho1090 <shaho.parvini@gmail.com>
2024-09-14 16:53:12 +0330

    lesson: 52, adding visual feedback to the buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     borderRadius: 8,
     elevation: 10,
     shadowColor: "black",
