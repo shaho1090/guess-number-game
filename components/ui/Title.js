1a023820cc5f278c8a52f28f02ba9a605d3231ff
shaho1090 <shaho.parvini@gmail.com>
2024-09-18 18:04:33 +0330

    lesson: 64, display random number with some refactoring

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,6 +1,6 @@
 import { Text, StyleSheet } from "react-native";
 
-import Colors from "../constants/colors";
+import Colors from "../../constants/colors";
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "bold",
-    color: Colors.accent500,
+    color: "white",
     textAlign: "center",
     borderWidth: 2,
     borderColor: Colors.accent500,
